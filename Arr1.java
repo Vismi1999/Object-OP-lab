798d6cc176e632ad3b66d758b6757a0a8047c5b9
Vismi1999 <98300328+Vismi1999@users.noreply.github.com>
2022-04-22 13:01:15 +0530

    Add files via upload

---
+++ Arr1.java
@@ -0,0 +1,10 @@
+class Arr1{
+	public static void main(String[]args)
+	{ 
+	 int[]age={1,3,5};
+	 System.out.println("elements of age");
+	 System.out.println("first elememt" +age[0]);
+	 System.out.println("second element" +age[1]);
+	 System.out.println("third elememt" +age[2]);
+	}
+}
