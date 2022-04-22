f22efbf94ac13b39ac1e2bb251f420fa0bb821ad
Vismi1999 <98300328+Vismi1999@users.noreply.github.com>
2022-04-22 13:01:30 +0530

    Add files via upload

---
+++ Largestelement.java
@@ -0,0 +1,14 @@
+class Sum{
+	public.static.void main(String[]args)
+	{
+		int[]array = newint{3,12,4,5,};
+		 for(i=0;i<array.length;i++)
+			 int sum=0;
+		  sum = sum+array[i];
+		  {
+			  System.out.println("Sum of all the elements of an array: " + sum);  
+		  }
+	}
+		  
+		 
+		
